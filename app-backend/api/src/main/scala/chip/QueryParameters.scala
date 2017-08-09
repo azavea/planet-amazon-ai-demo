b5249ae761077712ec5ed3bc83c9508ef245a833
yoninachmany <yoninachmany@gmail.com>
2017-08-09 13:47:00 -0400

    for Chips.listChips, added ChipQueryParameters, which required some API files

---
+++ app-backend/api/src/main/scala/chip/QueryParameters.scala
@@ -0,0 +1,19 @@
+package com.azavea.rf.api.chip
+
+import java.util.UUID
+import java.sql.Timestamp
+import java.time.Instant
+
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.{Directive1, Rejection}
+import akka.http.scaladsl.server.directives.ParameterDirectives.parameters
+import com.azavea.rf.database.query.ChipQueryParameters
+
+import com.azavea.rf.api.utils.queryparams._
+
+trait ChipQueryParameterDirective extends QueryParametersCommon {
+
+  val chipSpecificQueryParameters = parameters(
+    'sceneId.as[UUID].?
+  ).as(ChipQueryParameters.apply _)
+}
